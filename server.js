45329fa06132f0ac3cee7b51430fad48819f0cd6
saJeremyQin <343220868@qq.com>
2023-12-11 21:13:47 +1030

    Add the post message to array

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const messages = ["Tangseng","Suwukong","Zhubajie","Shawujing"];
+
 app.use(cors());
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send("Test page!");
 })
 
 app.get('/messages', (req, res) => {
-    const messages = ["Tangseng","Suwukong","Zhubajie","Shawujing"];
     res.send(messages);
+})
+
+app.post('/messages', (req, res) => {
+    console.log(req.body);
+    let msg = req.body;
+    messages.push(msg.message);
+    // res.send({messages});
+    res.json(msg);
+    console.log(messages);
 })
 
 app.listen(port, () => {
