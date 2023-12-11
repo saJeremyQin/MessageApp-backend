b38ec9a07c0d70b8877ccb2db01d75bf251676aa
saJeremyQin <343220868@qq.com>
2023-12-11 20:06:03 +1030

    siple server using cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.get('/', (req, res) => {
     res.send("Test page!");
 })
 
 app.get('/messages', (req, res) => {
-    const messages = ["first","second","Facts"];
+    const messages = ["Tangseng","Suwukong","Zhubajie","Shawujing"];
     res.send(messages);
 })
 
