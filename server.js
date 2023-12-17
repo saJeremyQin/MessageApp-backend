20cf0ca54b09908f1488c24bbab42559dd3201e7
saJeremyQin <343220868@qq.com>
2023-12-17 21:46:35 +1030

    with username

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-const messages = ["Tangseng","Suwukong","Zhubajie","Shawujing"];
-const users = [];
+const messages = [{user: "Jim", text: "Tangseng"},{user: "Jim", text: "Suwukong"},];
+const users = [{username:"Jim", password:"123"}];
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,8 +23,14 @@ app.get('/messages/:id', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-    let msg = req.body;
-    messages.push(msg.message);
+    const userId = req.header('Authorization');
+    const user = users[userId];
+    let msg = {
+        user: user.username,
+        text:req.body.message
+    }
+
+    messages.push(msg);
     res.json(msg);
 })
 
@@ -32,7 +38,8 @@ app.post('/register', (req, res) => {
     const registerData = req.body;
     users.push(registerData);
     console.log(users);
-    res.send(users);
+    registerData.id = users.length - 1;
+    res.send(registerData);
 })
 app.listen(port, () => {
     console.log(`App is runing at port ${port}`);
